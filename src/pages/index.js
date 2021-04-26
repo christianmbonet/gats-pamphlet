c41c4c85a28e93964936c1f9b100ab1d69315d1c
christianmbonet <christianm.bonet@gmail.com>
2021-04-26 19:33:08 -0400

    changed animoji file and setting up router

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 import './../components/index.css'
 import animoji from './../images/animoji.jpg'
+import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom"
 
 const IndexPage = () => (
   <Layout>
